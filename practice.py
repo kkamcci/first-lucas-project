4092c3907231851a401f982c110b737f3cf158ec
Lucas_Kim <kkamcci20@hotmail.com>
2023-07-24 13:54:31 -0700

    third commit

--- practice.py
+++ practice.py
@@ -1707,6 +1707,37 @@
 # print(dir(name))
 
 #외장 함수
+ #glob : 경로 내의 폴더 / 파일 목록 조회(윈도우 dir)
+# import glob
+# print(glob.glob("*.py")) #확정자가 py인 모든 파일에 대해서 알려달라.
 
 
-이부분 추가하기
+#os : 운영체제에서 제공하는 기본 기능
+#  
+# print(os.getcwd()) #현재 디렉토리를 표시해달라
+
+# folder = " sample_dir"
+
+# if os.path.exists(folder):
+#     print("이미 존재하는 폴더입니다.")
+#     os.rmdir(folder)
+#     print("폴더를 삭제하였습니다.")
+
+# else:
+#     os.makedirs(folder) #폴더 생성하기
+#     print(folder, "폴더를 생성하였습니다.")
+
+# print(os.listdir())
+
+#time : 시간 관련 함수
+# import time
+# print(time.localtime())
+# print(time.strftime("%m/%d/%y %H:%M:%S"))
+
+import datetime
+# print("오늘 날짜는 " , datetime.date.today())
+
+#timedelta :두 날짜 사이의 간격
+today = datetime.date.today()
+td = datetime.timedelta(days=100)
+print(" 우리가 만난지 100일은", today + td)
